2d2bb7afdbfc6a01f0f400807feb57bd3e10d6c0
Réparateur de JVC <105303775+DreamboxMinerva@users.noreply.github.com>
2026-05-10 23:40:55 +0200

    Update JVC-Redesign.user.js

--- JVC-Redesign.user.js
+++ JVC-Redesign.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JVC Redesign - Refonte de l'interface du forum
 // @namespace    http://tampermonkey.net/
-// @version      2.94
+// @version      3.0
 // @author       StrangerFruit + BlackArch + Bakuredo + captain_cid31 + herolink + Can-02
 // @description  Tentative de rendre l'UI le plus agréable possible
 // @match        https://www.jeuxvideo.com/forums/0-*
